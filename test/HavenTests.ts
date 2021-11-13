9d1b8c73f2b071a04a8189a8673795cfa99016bf
Erum <melbournebaldove@gmail.com>
2021-11-13 14:32:01 +0800

    Add test for contract ownership on post()

--- test/HavenTests.ts
+++ test/HavenTests.ts
@@ -1,16 +1,19 @@
 import { expect } from 'chai'
 import { ethers, waffle } from 'hardhat'
+import { Haven } from '../typechain'
 
 describe('Haven', function () {
-	describe('#post()', function () {
-		it('Should emit created post ', async function () {
-			const Haven = await ethers.getContractFactory('Haven')
-			const haven = await Haven.deploy(10)
-			await haven.deployed()
+	let haven: Haven
 
+	beforeEach(async function() {
+		const Haven = await ethers.getContractFactory('Haven')
+		haven = await Haven.deploy(10)
+		await haven.deployed()
+	})
+	describe('#post()', async function () {
+		it('Should emit created post ', async function () {
 			const postUri = 'some://posturi'
 			const postTx = await haven.post(postUri)
-
 			const receipt = await postTx.wait()
 			const currentBlock = await waffle.provider.getBlock(receipt.blockNumber)
 		
@@ -19,6 +22,16 @@ describe('Haven', function () {
 				.withArgs(0, postUri, currentBlock.timestamp)
 
 			expect((await haven.posts(0))[1]).to.equal(postUri)
+		})
+		it('Should allow only haven owner otherwise revert', async function() {
+			const [ _, signer1 ] = await ethers.getSigners()
+			const postUri = 'some://posturi'
+			const postTx = await haven.post(postUri)
+			await postTx.wait()
+
+			expect((await haven.posts(0))[1]).to.equal(postUri)
+
+			expect(await haven.connect(signer1).post(postUri)).to.be.reverted
 		})
 	})
 })
