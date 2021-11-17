51352f7741837a0210e441ba5ebdf47adbb33179
Erum <melbournebaldove@gmail.com>
2021-11-17 21:15:17 +0800

    Add deploy script for haven token

--- deploy/01_deploy_haven_protocol.ts
+++ deploy/01_deploy_haven_protocol.ts
@@ -4,10 +4,11 @@ import {DeployFunction} from 'hardhat-deploy/types'
 const func: DeployFunction = async ({getNamedAccounts, deployments}: HardhatRuntimeEnvironment) => {
 	const { deploy, log } = deployments
 	const { deployer } = await getNamedAccounts()
-
+	const HavenToken = await deployments.get('HavenToken')
 	log('----------------------------------------------------')
 	const havenProtocol = await deploy('HavenProtocol', {
 		from: deployer,
+		args: [HavenToken.address, 450], // 4.5%
 		log: true
 	})
 	log('HavenProtocol contract deployed at: ' + havenProtocol.address)
