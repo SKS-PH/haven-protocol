25187c53b0c3276ebe87c58201f30dd606e0f663
Andrew Concepcion <a-ar@outlook.com>
2021-11-28 04:30:36 +0800

    feat(WIP): ceramic setup

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,52 @@
 import fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify';
 import { Server, IncomingMessage, ServerResponse } from 'http';
+import { CeramicClient } from '@ceramicnetwork/http-client'
+import KeyDidResolver from '@ceramicnetwork/key-did-resolver'
+import ThreeIdResolver from '@ceramicnetwork/3id-did-resolver'
+import ThreeIdProvider from '3id-did-provider'
+import GetPermissionFn from '3id-did-provider'
+import { randomBytes } from '@stablelib/random'
+import { DID, DIDOptions, ResolverRegistry } from 'dids'
+import { Ed25519Provider } from 'key-did-provider-ed25519'
+import { TileDocument } from '@ceramicnetwork/stream-tile'
+
+const CERAMIC_API_URL = 'http://localhost:7007'
+const ceramic = new CeramicClient(CERAMIC_API_URL)
+
+let posts: any = []
+
+const didSetup = async () => {
+  const authId = 'myAuthenticationMethod' // a name of the auth method
+  const authSecret = randomBytes(32)
+  const getPermission = async (request: {
+      type: string;
+      origin?: string | null;
+      payload: Record<string, any>;
+  }) => {
+    return request.payload.paths
+  }
+
+  const threeId = await ThreeIdProvider.create({ 
+    authId: 'genesis',
+    authSecret,
+    getPermission, 
+    ceramic 
+  })
+
+  const resolver: any = { 
+    ...KeyDidResolver.getResolver(),
+    ...ThreeIdResolver.getResolver(ceramic) 
+  }
+  const provider: any = threeId.getDidProvider()
+  const did = new DID({ resolver })
+  did.setProvider(provider)
+  ceramic.did = did
+
+  ceramic.did.authenticate()
+
+}
+
+// didSetup()
 
 const server: FastifyInstance<
   Server,
@@ -15,19 +62,6 @@ interface PostHeaders {}
 interface PostBody {
   post?: object;
 }
-
-const opts: RouteShorthandOptions = {
-  schema: {
-    body: {
-      type: 'object',
-      properties: {
-        pong: {
-          type: 'string'
-        }
-      }
-    }
-  }
-};
 
 const post: RouteShorthandOptions = {
   schema: {
@@ -53,9 +87,12 @@ server.post<{
   Params: PostParams;
   Headers: PostHeaders;
   Body: PostBody;
-}>('/posts', post, (request, reply) => {
+}>('/haven/:havenId/posts', post, (request, reply) => {
   console.log(request.body);
-  reply.code(200).send({ post: 'should save to db' });
+  console.log(request.params);
+  posts.push(request.body)
+  // should save to ceramic
+  reply.code(200).send({ post: 'should save to ceramic' });
 });
 
 
@@ -63,13 +100,22 @@ server.get<{
   Querystring: PostQuerystring;
   Params: PostParams;
   Headers: PostHeaders;
-}>('/posts', (request, reply) => {
-  reply.code(200).send({ posts: 'should return array of posts' });
-
+}>('/haven/:havenId/posts', (request, reply) => {
+  // should come from ceramic
+  reply.code(200).send(posts);
 });
 
+server.get<{
+  Querystring: PostQuerystring;
+  Params: PostParams;
+  Headers: PostHeaders;
+}>('/subscriptions', (request, reply) => {
+  reply.code(200).send({ posts: 'should return array of havens' });
+});
+
+
 // Start your server
-server.listen(8080, (err, address) => {
+server.listen(8080, async (err, address) => {
   if (err) {
     console.error(err);
   }
