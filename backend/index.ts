b8d1a0075ba6f3e8c57c618f773d25609e4ae92a
Erum <melbournebaldove@gmail.com>
2021-11-28 13:23:15 +0800

    Fix ceramic crash

--- backend/index.ts
+++ backend/index.ts
@@ -1,122 +1,133 @@
-import fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify';
-import { Server, IncomingMessage, ServerResponse } from 'http';
-import { CeramicClient } from '@ceramicnetwork/http-client'
-import KeyDidResolver from '@ceramicnetwork/key-did-resolver'
+import fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify'
+import { Server, IncomingMessage, ServerResponse } from 'http'
+import {Ceramic} from '@ceramicnetwork/core'
+import IPFS from 'ipfs-core'
+import dagJose from 'dag-jose'
+import { convert } from 'blockcodec-to-ipld-format'
+import KeyDidResolver from 'key-did-resolver'
 import ThreeIdResolver from '@ceramicnetwork/3id-did-resolver'
 import ThreeIdProvider from '3id-did-provider'
-import GetPermissionFn from '3id-did-provider'
+const dagJoseFormat = convert(dagJose.default)
 import { randomBytes } from '@stablelib/random'
-import { DID, DIDOptions, ResolverRegistry } from 'dids'
-import { Ed25519Provider } from 'key-did-provider-ed25519'
-import { TileDocument } from '@ceramicnetwork/stream-tile'
+import { DID, DIDOptions } from 'dids'
+
 
 const CERAMIC_API_URL = 'http://localhost:7007'
-const ceramic = new CeramicClient(CERAMIC_API_URL)
 
-let posts: any = []
+const ipfs = await IPFS.create({ ipld: { formats: [dagJoseFormat] } })
 
+const ceramic = await Ceramic.create(ipfs)
+
+const resolver = {
+	...KeyDidResolver.getResolver(),
+	...ThreeIdResolver.getResolver(ceramic),
+}
+const did = new DID({ resolver })
+ceramic.did = did
+const posts: any = []
+console.log(ThreeIdProvider)
 const didSetup = async () => {
-  const authId = 'myAuthenticationMethod' // a name of the auth method
-  const authSecret = randomBytes(32)
-  const getPermission = async (request: {
-      type: string;
-      origin?: string | null;
-      payload: Record<string, any>;
-  }) => {
-    return request.payload.paths
-  }
+	const authId = 'myAuthenticationMethod' // a name of the auth method
+	const authSecret = randomBytes(32)
+	const getPermission = async (request: {
+		type: string;
+		origin?: string | null;
+		payload: Record<string, any>;
+	}) => {
+		return request.payload.paths
+	}
 
-  const threeId = await ThreeIdProvider.create({ 
-    authId: 'genesis',
-    authSecret,
-    getPermission, 
-    ceramic 
-  })
+	const threeId = await ThreeIdProvider.default.create({ 
+		authId: 'genesis',
+		authSecret,
+		getPermission, 
+		ceramic 
+	})
 
-  const resolver: any = { 
-    ...KeyDidResolver.getResolver(),
-    ...ThreeIdResolver.getResolver(ceramic) 
-  }
-  const provider: any = threeId.getDidProvider()
-  const did = new DID({ resolver })
-  did.setProvider(provider)
-  ceramic.did = did
+	const resolver: any = { 
+		...KeyDidResolver.getResolver(),
+		...ThreeIdResolver.getResolver(ceramic) 
+	}
+	const provider: any = threeId.getDidProvider()
+	const did = new DID({ resolver })
+	did.setProvider(provider)
+	ceramic.did = did
 
-  ceramic.did.authenticate()
+	ceramic.did.authenticate()
 
 }
 
-// didSetup()
+await didSetup()
 
 const server: FastifyInstance<
-  Server,
-  IncomingMessage,
-  ServerResponse
-> = fastify({ logger: true });
+Server,
+IncomingMessage,
+ServerResponse
+> = fastify({ logger: true })
 
 interface PostQuerystring {}
 interface PostParams {
-  params?: string;
+	params?: string;
 }
 interface PostHeaders {}
 interface PostBody {
-  post?: object;
+	post?: object;
 }
 
 const post: RouteShorthandOptions = {
-  schema: {
-    body: {
-      type: 'object',
-      properties: {
-          id: { type: 'string' },
-          createdAt: { type: 'string', format: 'date' },
-          post: { type: 'string' },
-          title: { type: 'string' },
-          attachments: { type: 'array', items: { type: 'string' } },
-          tags: { type: 'array', items: { type: 'string' } },
-          people: { type: 'array', items: { type: 'string' } },
-          comments: { type: 'array', items: { type: 'string' } },
-          likesAddresses: { type: 'array', items: { type: 'string' } },
-      }
-    }
-  }
+	schema: {
+		body: {
+			type: 'object',
+			properties: {
+				id: { type: 'string' },
+				createdAt: { type: 'string', format: 'date' },
+				post: { type: 'string' },
+				title: { type: 'string' },
+				attachments: { type: 'array', items: { type: 'string' } },
+				tags: { type: 'array', items: { type: 'string' } },
+				people: { type: 'array', items: { type: 'string' } },
+				comments: { type: 'array', items: { type: 'string' } },
+				likesAddresses: { type: 'array', items: { type: 'string' } },
+			}
+		}
+	}
 }
 
 server.post<{
-  Querystring: PostQuerystring;
-  Params: PostParams;
-  Headers: PostHeaders;
-  Body: PostBody;
+	Querystring: PostQuerystring;
+	Params: PostParams;
+	Headers: PostHeaders;
+	Body: PostBody;
 }>('/haven/:havenId/posts', post, (request, reply) => {
-  console.log(request.body);
-  console.log(request.params);
-  posts.push(request.body)
-  // should save to ceramic
-  reply.code(200).send({ post: 'should save to ceramic' });
-});
+	console.log(request.body)
+	console.log(request.params)
+	posts.push(request.body)
+	// should save to ceramic
+	reply.code(200).send({ post: 'should save to ceramic' })
+})
 
 
 server.get<{
-  Querystring: PostQuerystring;
-  Params: PostParams;
-  Headers: PostHeaders;
+	Querystring: PostQuerystring;
+	Params: PostParams;
+	Headers: PostHeaders;
 }>('/haven/:havenId/posts', (request, reply) => {
-  // should come from ceramic
-  reply.code(200).send(posts);
-});
+	// should come from ceramic
+	reply.code(200).send(posts)
+})
 
 server.get<{
-  Querystring: PostQuerystring;
-  Params: PostParams;
-  Headers: PostHeaders;
+	Querystring: PostQuerystring;
+	Params: PostParams;
+	Headers: PostHeaders;
 }>('/subscriptions', (request, reply) => {
-  reply.code(200).send({ posts: 'should return array of havens' });
-});
+	reply.code(200).send({ posts: 'should return array of havens' })
+})
 
 
 // Start your server
 server.listen(8080, async (err, address) => {
-  if (err) {
-    console.error(err);
-  }
-});
+	if (err) {
+		console.error(err)
+	}
+})
