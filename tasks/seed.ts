b1e4ac6d5fa985752ef447a71ae3a24cba093900
Erum <melbournebaldove@gmail.com>
2021-11-28 23:39:13 +0800

    Seed post for haven

--- tasks/seed.ts
+++ tasks/seed.ts
@@ -1,4 +1,8 @@
 import { task } from 'hardhat/config'
+<<<<<<< Updated upstream
+=======
+import fetch from 'node-fetch'
+>>>>>>> Stashed changes
 
 task('seed', 'Seeds onchain and offchain', async (taskArgs, hre, runSuper) => {
 	const network = hre.network
@@ -11,7 +15,6 @@ task('seed', 'Seeds onchain and offchain', async (taskArgs, hre, runSuper) => {
 		const havenProtocolAddress = (await deployments.get('HavenProtocol')).address
 		const HavenProtocol = await ethers.getContractFactory('HavenProtocol')
 		const HavenToken = await ethers.getContractFactory('HavenToken')
-		const Haven = await ethers.getContractFactory('Haven')
 		const havenToken = HavenToken.attach(havenTokenAddress)
 		const havenProtocol = HavenProtocol.attach(havenProtocolAddress)
 		console.log('-------Deploying a haven-------')
@@ -29,6 +32,27 @@ task('seed', 'Seeds onchain and offchain', async (taskArgs, hre, runSuper) => {
 		await havenToken.connect(account1).authorizeOperator(havenProtocolAddress)
 		await havenProtocol.connect(account1).subscribe(havenId)
 		console.log('Subscribed!')
+
+		console.log('Posting for haven: %s...', havenId)
+		const post = {
+			id: '1',
+			createdAt: new Date('2021-11-24T22:09:46.000Z'),
+			content: `Hello folks, stay tuned for the album I'm uploading this month. I assure you, this new content is a real banger!
+
+Share the word to your friends so they can listen to the hard work I've been making for these past few months.
+
+Oh, and also for the Tier 2 subscribers and higher, hang on for a while because I will send extra merch to this haven. **More reasons** to get the higher tiers!`,
+			title: 'I am releasing a new album this month',
+			tags: ['album', 'release'],
+			tier: 'Tier 1',
+			haven: havenId
+		}
+		const response = await fetch(`http://localhost:8080/api/haven/${havenId}/posts`, { method: 'POST', body: JSON.stringify(post), headers: { 'Content-Type': 'application/json' }})
+		const data = await response.json()
+
+		const postTx = await havenProtocol.post(0, data.streamId)
+		await postTx.wait()
+
 	}
 })
 
